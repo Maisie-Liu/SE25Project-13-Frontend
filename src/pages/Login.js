6caec178c37b9911f7ea8abf0be837699704f885
Maisie-Liu <1014889214@qq.com>
2025-10-17 12:30:40 +0800

    change Login.js for test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -393,7 +393,7 @@ const Login = () => {
             <Divider />
             
             <div style={{ textAlign: 'center' }}>
-              <Text>还没有账号？</Text> <RegisterLink to="/register">立即注册</RegisterLink>
+              <Text>test还没有账号？</Text> <RegisterLink to="/register">立即注册</RegisterLink>
             </div>
           </FormContainer>
         </RightPanel>
